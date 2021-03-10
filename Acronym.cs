6f847327df0e5db24d16698bb5d886e1f7d82760
shmirgelis <73270957+shmirgelis@users.noreply.github.com>
2021-03-10 09:20:52 +0200

    Add project files.

---
+++ Acronym.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        char[] delimitterChars = { ' ', ',', '.', '-', '_' };
+        string[] words = phrase.Split(delimitterChars);
+        string acronym = "";
+        foreach (string word in words)
+        {
+            if (word == "")   // cia turbut galima sutrumpint if statementa, bet nebepamenu kaip :)
+            {
+            }
+            else
+            {
+                acronym += word[0].ToString().ToUpper();
+            }
+        }
+        return acronym;
+    }
+}
